0eae520aac4018cdba6bbc1ad683a8c28a3905aa
harisfi <acc.cp.haris@gmail.com>
2021-03-29 14:42:36 +0700

    Assign project login and edit profile

--- admin/profil.php
+++ admin/profil.php
@@ -1,79 +1,96 @@
+<?php
+session_start();
+include('../koneksi/koneksi.php');
+$id_user = $_SESSION['id_user'];
+//get profil
+$sql = "select `nama`, `email`,`foto` from `user` where `id_user`='$id_user'";
+//echo $sql;
+$query = mysqli_query($koneksi, $sql);
+while ($data = mysqli_fetch_row($query)) {
+  $nama = $data[0];
+  $email = $data[1];
+  $foto = $data[2];
+}
+?>
 <!DOCTYPE html>
 <html>
+
 <head>
-<?php include("includes/head.php") ?> 
+  <?php include("includes/head.php") ?>
 </head>
+
 <body class="hold-transition sidebar-mini layout-fixed">
-<div class="wrapper">
-<?php include("includes/header.php") ?>
+  <div class="wrapper">
+    <?php include("includes/header.php") ?>
 
-  <?php include("includes/sidebar.php") ?>
+    <?php include("includes/sidebar.php") ?>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h3><i class="fas fa-user-tie"></i> Profil</h3>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item active">Profil</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-            <div class="card">
-              <div class="card-header">
-                <div class="card-tools">
-                  <a href="editprofil.php" class="btn btn-sm btn-info float-right"><i class="fas fa-edit"></i> Edit Profil</a>
-                </div>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                <div class="col-sm-12">
-                    <div class="alert alert-success" role="alert">Data Berhasil Diubah</div>
-                </div>
-                <table class="table table-bordered">
-                    <tbody>  
-                      <tr>
-                        <td colspan="2"><i class="fas fa-user-circle"></i> <strong>PROFIL<strong></td>
-                      </tr> 
-                      <tr>
-                        <td width="20%"><strong>Foto<strong></td>
-                        <td width="80%"><img src="foto/salnan.jpg" class="img-fluid" width="200px;"></td>
-                      </tr>                
-                      <tr>
-                        <td width="20%"><strong>Nama<strong></td>
-                        <td width="80%">Salnan Ratih A.</td>
-                      </tr>                
-                      <tr>
-                        <td width="20%"><strong>Email<strong></td>
-                        <td width="80%">salnanratih@gmail.com</td>
-                      </tr> 
-                    </tbody>
-                  </table>  
-              </div>
-              <!-- /.card-body -->
-              <div class="card-footer clearfix">&nbsp;</div>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h3><i class="fas fa-user-tie"></i> Profil</h3>
             </div>
-            <!-- /.card -->
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item active">Profil</li>
+              </ol>
+            </div>
+          </div>
+        </div><!-- /.container-fluid -->
+      </section>
 
-    </section>
-    <!-- /.content -->
+      <!-- Main content -->
+      <section class="content">
+        <div class="card">
+          <div class="card-header">
+            <div class="card-tools">
+              <a href="editprofil.php" class="btn btn-sm btn-info float-right"><i class="fas fa-edit"></i> Edit Profil</a>
+            </div>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body">
+            <div class="col-sm-12">
+              <div class="alert alert-success" role="alert">Data Berhasil Diubah</div>
+            </div>
+            <table class="table table-bordered">
+              <tbody>
+                <tr>
+                  <td colspan="2"><i class="fas fa-user-circle"></i> <strong>PROFIL<strong></td>
+                </tr>
+                <tr>
+                  <td width="20%"><strong>Foto<strong></td>
+                  <td width="80%"><img src="foto/<?=$foto?>" class="img-fluid" width="200px;"></td>
+                </tr>
+                <tr>
+                  <td width="20%"><strong>Nama<strong></td>
+                  <td width="80%"><?=$nama?></td>
+                </tr>
+                <tr>
+                  <td width="20%"><strong>Email<strong></td>
+                  <td width="80%"><?=$email?></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <!-- /.card-body -->
+          <div class="card-footer clearfix">&nbsp;</div>
+        </div>
+        <!-- /.card -->
+
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+    <?php include("includes/footer.php") ?>
+
   </div>
-  <!-- /.content-wrapper -->
-  <?php include("includes/footer.php") ?>
+  <!-- ./wrapper -->
 
-</div>
-<!-- ./wrapper -->
-
-<?php include("includes/script.php") ?>
+  <?php include("includes/script.php") ?>
 </body>
+
 </html>
