0aaafb47470c3d3ecfab6304c8de1c5480f24e09
harisfi <acc.cp.haris@gmail.com>
2021-03-30 22:47:23 +0700

    Add CRUD for penerbit section

---
+++ admin/konfirmasieditpenerbit.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+include('../koneksi/koneksi.php');
+if (isset($_SESSION['id_penerbit'])) {
+    $id_penerbit = $_SESSION['id_penerbit'];
+    $penerbit = $_POST['penerbit'];
+    $alamat = $_POST['alamat'];
+    if (empty($penerbit) || empty($alamat)) {
+        header("Location:editpenerbit.php?data=" . $id_penerbit . "&notif=editkosong");
+    } else {
+        $sql = "update `penerbit` set `penerbit`='$penerbit',`alamat`='$alamat' where `id_penerbit`='$id_penerbit'";
+        mysqli_query($koneksi, $sql);
+        unset($_SESSION['id_penerbit']);
+        header("Location:penerbit.php?notif=editberhasil");
+    }
+}
