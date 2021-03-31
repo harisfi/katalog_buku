dc8236b5d3ed9c811d0e4ac1fa69c38bc65abdb5
harisfi <acc.cp.haris@gmail.com>
2021-03-31 09:06:11 +0700

    Implement more restrictions

--- admin/includes/auth.php
+++ admin/includes/auth.php
@@ -1,7 +1,18 @@
 <?php
 session_start();
+$now = basename($_SERVER['PHP_SELF']);
 if (empty($_SESSION['id_user']) || empty($_SESSION['level'])) {
     if (basename($_SERVER['PHP_SELF'])!="index.php") {
         header("Location:index.php");
+    }
+} else {
+    if ($now == "index.php") {
+        header("Location:profil.php");
+    }
+
+    if ($_SESSION['level'] != "Superadmin") {
+        if ($now == "user.php" || $now == "edituser.php" || $now == "tambahuser.php" || $now == "detailuser.php") {
+            header("Location:profil.php");
+        }
     }
 }
