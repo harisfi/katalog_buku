f712ef6a62b9dacd25efb289194baf96f9b20ccb
harisfi <acc.cp.haris@gmail.com>
2021-03-31 08:34:19 +0700

    Add CRUD for blog section

--- admin/detailblog.php
+++ admin/detailblog.php
@@ -1,91 +1,102 @@
+<?php
+include('../koneksi/koneksi.php');
+if (isset($_GET['data'])) {
+  $id_blog = $_GET['data'];
+  $sql_k = "SELECT tanggal, (SELECT kategori_blog FROM kategori_blog WHERE id_kategori_blog = blog.id_kategori_blog), judul, (SELECT nama FROM user WHERE id_user = blog.id_user), isi FROM `blog` WHERE `id_blog` = '$id_blog'";
+  $query_k = mysqli_query($koneksi, $sql_k);
+  while ($data_k = mysqli_fetch_row($query_k)) {
+    $tanggal = $data_k[0];
+    $kategori = $data_k[1];
+    $judul = $data_k[2];
+    $penulis = $data_k[3];
+    $isi = $data_k[4];
+  }
+}
+?>
 <!DOCTYPE html>
 <html>
+
 <head>
-<?php include("includes/head.php") ?> 
+  <?php include("includes/head.php") ?>
 </head>
+
 <body class="hold-transition sidebar-mini layout-fixed">
-<div class="wrapper">
-<?php include("includes/header.php") ?>
+  <div class="wrapper">
+    <?php include("includes/header.php") ?>
 
-  <?php include("includes/sidebar.php") ?>
+    <?php include("includes/sidebar.php") ?>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h3><i class="fas fa-user-tie"></i> Detail Data Blog</h3>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item"><a href="blog.php">Data Blog</a></li>
-              <li class="breadcrumb-item active">Detail Data Blog</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-            <div class="card">
-              <div class="card-header">
-                <div class="card-tools">
-                  <a href="blog.php" class="btn btn-sm btn-warning float-right">
-                  <i class="fas fa-arrow-alt-circle-left"></i> Kembali</a>
-                </div>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                <table class="table table-bordered">
-                    <tbody>                 
-                      <tr>
-                        <td width="20%"><strong>Tanggal<strong></td>
-                        <td width="80%">24-2-2021</td>
-                      </tr>              
-                      <tr>
-                        <td width="20%"><strong>Kategori Blog<strong></td>
-                        <td width="80%">Teknologi</td>
-                      </tr>                 
-                      <tr>
-                        <td width="20%"><strong>Judul<strong></td>
-                        <td width="80%">Teknologi Terkini</td>
-                      </tr> 
-                      <tr>
-                        <td width="20%"><strong>Penulis<strong></td>
-                        <td width="80%">Salnan Ratih</td>
-                      </tr>
-                      <tr>
-                        <td width="20%"><strong>Isi<strong></td>
-                        <td width="80%">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                        when an unknown printer took a galley of type and scrambled it to make a type 
-                        specimen book. It has survived not only five centuries, but also the leap into 
-                        electronic typesetting, remaining essentially unchanged. It was popularised in the
-                         1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
-                          recently with desktop publishing software like Aldus PageMaker including versions
-                           of Lorem Ipsum.</td>
-                      </tr>
-                    </tbody>
-                  </table>  
-              </div>
-              <!-- /.card-body -->
-              <div class="card-footer clearfix">&nbsp;</div>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h3><i class="fas fa-user-tie"></i> Detail Data Blog</h3>
             </div>
-            <!-- /.card -->
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item"><a href="blog.php">Data Blog</a></li>
+                <li class="breadcrumb-item active">Detail Data Blog</li>
+              </ol>
+            </div>
+          </div>
+        </div><!-- /.container-fluid -->
+      </section>
 
-    </section>
-    <!-- /.content -->
+      <!-- Main content -->
+      <section class="content">
+        <div class="card">
+          <div class="card-header">
+            <div class="card-tools">
+              <a href="blog.php" class="btn btn-sm btn-warning float-right">
+                <i class="fas fa-arrow-alt-circle-left"></i> Kembali</a>
+            </div>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body">
+            <table class="table table-bordered">
+              <tbody>
+                <tr>
+                  <td width="20%"><strong>Tanggal<strong></td>
+                  <td width="80%"><?= $tanggal ?></td>
+                </tr>
+                <tr>
+                  <td width="20%"><strong>Kategori Blog<strong></td>
+                  <td width="80%"><?= $kategori ?></td>
+                </tr>
+                <tr>
+                  <td width="20%"><strong>Judul<strong></td>
+                  <td width="80%"><?= $judul ?></td>
+                </tr>
+                <tr>
+                  <td width="20%"><strong>Penulis<strong></td>
+                  <td width="80%"><?= $penulis ?></td>
+                </tr>
+                <tr>
+                  <td width="20%"><strong>Isi<strong></td>
+                  <td width="80%"><?= $isi ?></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <!-- /.card-body -->
+          <div class="card-footer clearfix">&nbsp;</div>
+        </div>
+        <!-- /.card -->
+
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+    <?php include("includes/footer.php") ?>
+
   </div>
-  <!-- /.content-wrapper -->
-  <?php include("includes/footer.php") ?>
+  <!-- ./wrapper -->
 
-</div>
-<!-- ./wrapper -->
-
-<?php include("includes/script.php") ?>
+  <?php include("includes/script.php") ?>
 </body>
+
 </html>
--- admin/editblog.php
+++ admin/editblog.php
@@ -1,100 +1,131 @@
+<?php
+session_start();
+include('../koneksi/koneksi.php');
+if (isset($_GET['data'])) {
+  $id_blog = $_GET['data'];
+  $_SESSION['id_blog'] = $id_blog;
+  //get data kategori buku
+  $sql_d = "select (select kategori_blog from kategori_blog where id_kategori_blog = blog.id_kategori_blog), `judul`,`isi` from `blog` where `id_blog` = '$id_blog'";
+  $query_d = mysqli_query($koneksi, $sql_d);
+  while ($data_d = mysqli_fetch_row($query_d)) {
+    $kategori = $data_d[0];
+    $judul = $data_d[1];
+    $isi = $data_d[2];
+  }
+}
+?>
 <!DOCTYPE html>
 <html>
+
 <head>
-<?php include("includes/head.php") ?> 
+  <?php include("includes/head.php") ?>
 </head>
+
 <body class="hold-transition sidebar-mini layout-fixed">
-<div class="wrapper">
-<?php include("includes/header.php") ?>
+  <div class="wrapper">
+    <?php include("includes/header.php") ?>
 
-  <?php include("includes/sidebar.php") ?>
+    <?php include("includes/sidebar.php") ?>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h3><i class="fas fa-edit"></i> Edit Data Blog</h3>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="blog.php">Data Blog</a></li>
-              <li class="breadcrumb-item active">Edit Data Blog</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-
-    <div class="card card-info">
-      <div class="card-header">
-        <h3 class="card-title"style="margin-top:5px;"><i class="far fa-list-alt"></i> Form Edit Data Blog</h3>
-        <div class="card-tools">
-          <a href="blog.php" class="btn btn-sm btn-warning float-right">
-          <i class="fas fa-arrow-alt-circle-left"></i> Kembali</a>
-        </div>
-      </div>
-      <!-- /.card-header -->
-      <!-- form start -->
-      </br></br>
-      <div class="col-sm-10">
-          <div class="alert alert-danger" role="alert">Maaf judul wajib di isi</div>
-      </div>
-      <form class="form-horizontal">
-        <div class="card-body">
-          
-        <div class="form-group row">
-            <label for="kategori" class="col-sm-3 col-form-label">Kategori Blog</label>
-            <div class="col-sm-7">
-              <select class="form-control" id="kategori">
-                <option value="0">- Pilih Kategori -</option>
-                <option value="Teknologi" selected>Teknologi</option>
-                <option value="Pemrograman">Pemrograman</option>
-              </select>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h3><i class="fas fa-edit"></i> Edit Data Blog</h3>
+            </div>
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="blog.php">Data Blog</a></li>
+                <li class="breadcrumb-item active">Edit Data Blog</li>
+              </ol>
             </div>
           </div>
-          <div class="form-group row">
-            <label for="nim" class="col-sm-3 col-form-label">Judul</label>
-            <div class="col-sm-7">
-              <input type="text" class="form-control" name="nim" id="nim" value="Tekologi Terbaru">
+        </div><!-- /.container-fluid -->
+      </section>
+
+      <!-- Main content -->
+      <section class="content">
+
+        <div class="card card-info">
+          <div class="card-header">
+            <h3 class="card-title" style="margin-top:5px;"><i class="far fa-list-alt"></i> Form Edit Data Blog</h3>
+            <div class="card-tools">
+              <a href="blog.php" class="btn btn-sm btn-warning float-right">
+                <i class="fas fa-arrow-alt-circle-left"></i> Kembali</a>
             </div>
           </div>
-          <div class="form-group row">
-            <label for="isi" class="col-sm-3 col-form-label">Isi</label>
-            <div class="col-sm-7">
-              <textarea class="form-control" name="isi" id="editor1" rows="12">Lorem Ipsum</textarea>
+          <!-- /.card-header -->
+          <!-- form start -->
+          </br></br>
+          <div class="col-sm-10">
+            <?php if (!empty($_GET['notif'])) { ?>
+              <?php if ($_GET['notif'] == "editkosong") { ?>
+                <div class="alert alert-danger" role="alert">
+                  Maaf data Blog wajib di isi</div>
+              <?php } ?>
+            <?php } ?>
+          </div>
+          <form class="form-horizontal" method="POST" action="konfirmasieditblog.php">
+            <div class="card-body">
+
+              <div class="form-group row">
+                <label for="kategori" class="col-sm-3 col-form-label">Kategori Blog</label>
+                <div class="col-sm-7">
+                  <select class="custom-select" id="kategori" name="kategori">
+                    <option value="0">- Pilih Kategori -</option>
+                    <?php
+                    $sql_k = "SELECT id_kategori_blog, kategori_blog FROM kategori_blog";
+                    $query_k = mysqli_query($koneksi, $sql_k);
+                    while ($data_k = mysqli_fetch_row($query_k)) {
+                      $id_kategori_blog = $data_k[0];
+                      $kategori_blog = $data_k[1];
+                    ?>
+                      <option value="<?= $id_kategori_blog ?>" <?= ($kategori == $kategori_blog) ? "selected" : "" ?>><?= $kategori_blog ?></option>
+                    <?php } ?>
+                  </select>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="judul" class="col-sm-3 col-form-label">Judul</label>
+                <div class="col-sm-7">
+                  <input type="text" class="form-control" name="judul" id="judul" value="<?= $judul ?>">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="isi" class="col-sm-3 col-form-label">Isi</label>
+                <div class="col-sm-7">
+                  <textarea class="form-control" name="isi" id="editor1" rows="12"><?= $isi ?></textarea>
+                </div>
+              </div>
+
             </div>
-          </div>
-
-          </div>
         </div>
 
-      </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-          <div class="col-sm-12">
-            <button type="submit" class="btn btn-info float-right"><i class="fas fa-save"></i> Simpan</button>
-          </div>  
-        </div>
-        <!-- /.card-footer -->
-      </form>
     </div>
-    <!-- /.card -->
+    <!-- /.card-body -->
+    <div class="card-footer">
+      <div class="col-sm-12">
+        <button type="submit" class="btn btn-info float-right"><i class="fas fa-save"></i> Simpan</button>
+      </div>
+    </div>
+    <!-- /.card-footer -->
+    </form>
+  </div>
+  <!-- /.card -->
 
-    </section>
-    <!-- /.content -->
+  </section>
+  <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
   <?php include("includes/footer.php") ?>
 
-</div>
-<!-- ./wrapper -->
+  </div>
+  <!-- ./wrapper -->
 
-<?php include("includes/script.php") ?>
+  <?php include("includes/script.php") ?>
 </body>
+
 </html>
