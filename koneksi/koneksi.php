0013970937b80b3e91a8ec0aa1aa4cc2b38f102b
harisfi <acc.cp.haris@gmail.com>
2021-04-12 06:03:13 +0700

    Move into local database

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -1,6 +1,5 @@
 <?php
-$koneksi = mysqli_connect("sql6.freemysqlhosting.net", "sql6402829", "bAGf1SCDs1", "sql6402829");
-// $koneksi = mysqli_connect("localhost", "root", "", "katalog_buku");
+$koneksi = mysqli_connect("localhost", "root", "", "katalog_buku");
 // cek koneksi
 if (!$koneksi){
     die("Error koneksi: " . mysqli_connect_errno());
