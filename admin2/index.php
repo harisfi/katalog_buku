b70541b1fe28909a0c3d295691c919686980b2ee
alfarobi695 <81664613+alfarobi695@users.noreply.github.com>
2021-04-27 15:41:47 +0700

    Update index.php

--- admin2/index.php
+++ admin2/index.php
@@ -8,8 +8,25 @@
   if (isset($_GET["include"])) {
     $include = $_GET["include"];
     if (isset($_SESSION["id_user"])) {
-      $incval = array("blog", "buku", "konten", "penerbit", "tag", "user", "detailblog");
-      $realpg = array("blog", "buku", "konten", "penerbit", "tag", "user", "detail-blog");
+      $incval = array("blog", "buku", "konten", "penerbit", "tag", "user", 
+                      "detailblog", "editblog", "editkategoriblog", "kategoriblog", "konfirmasieditblog", "konfirmasieditkategoriblog", "konfirmasitambahblog", 
+                      "konfirmasitambahkategoriblog", "tambahblog", "tambahkategoriblog", 
+                      "detailbuku", "editbuku", "editkategoribuku", "kategoribuku", "konfirmasieditbuku", "konfirmasieditkategoribuku", "konfirmasitambahbuku", 
+                      "konfirmasitambahkategoribuku", "tambahbuku", "tambahkategoribuku", 
+                      "detailkonten", "editkonten", "konfirmasieditkonten", "konfirmasitambahkonten", "tambahkonten", 
+                      "editpenerbit", "konfirmasieditpenerbit", "konfirmasitambahpenerbit", "tambahpenerbit", 
+                      "edittag", "konfirmasiedittag", "konfirmasitambahtag", "tambahtag", 
+                      "detailuser", "edituser", "konfirmasiedituser", "konfirmasitambahuser", "tambahuser");
+      
+      $realpg = array("blog", "buku", "konten", "penerbit", "tag", "user", 
+                      "detai-lblog", "edit-blog", "edit-kategori-blog", "kategori-blog", "konfirmasi-edit-blog", "konfirmasi-edit-kategori-blog", "konfirmasi-tambah-blog", 
+                      "konfirmasi-tambah-kategori-blog", "tambah-blog", "tambah-kategori-blog", 
+                      "detail-buku", "edit-buku", "edit-kategori-buku", "kategori-buku", "konfirmasi-edit-buku", "konfirmasi-edit-kategori-buku", "konfirmasi-tambah-buku", 
+                      "konfirmasi-tambah-kategori-buku", "tambah-buku", "tambah-kategori-buku", 
+                      "detail-konten", "edit-konten", "konfirmasi-edit-konten", "konfirmasi-tambah-konten", "tambah-konten", 
+                      "edit-penerbit", "konfirmasi-edit-penerbit", "konfirmasi-tambah-penerbit", "tambah-penerbit", 
+                      "edit-tag", "konfirmasi-edit-tag", "konfirmasi-tambah-tag", "tambah-tag", 
+                      "detail-user", "edit-user", "konfirmasi-edit-user", "konfirmasi-tambah-user", "tambah-user");
 
       if ($include == "kategori-buku") {
         include("include/kategoribuku.php");
